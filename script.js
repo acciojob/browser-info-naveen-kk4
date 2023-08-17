c2f5706862d4a1264267d56b5946df2260fb2b9e
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-18 03:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+let appName = window.navigator.appName;
+let version = window.navigator.appVersion;
+document.getElementById("browser-info").innerHTML = `You are using ${appName} version ${version}`
